eb94b1e8f3f23c82ba16f6fe3e397a124efce1cf
Pavel Kozlov <gravizappa0@gmail.com>
2014-02-19 09:59:30 +0100

    Initial Commit

---
+++ MongoApp/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  MongoApp
+//
+//  Created by Pavel Kozlov on 19.02.14.
+//  Copyright (c) 2014 Pavel Kozlov. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ MongoAppTests/MongoAppTests.m
@@ -0,0 +1,34 @@
+//
+//  MongoAppTests.m
+//  MongoAppTests
+//
+//  Created by Pavel Kozlov on 19.02.14.
+//  Copyright (c) 2014 Pavel Kozlov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MongoAppTests : XCTestCase
+
+@end
+
+@implementation MongoAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
