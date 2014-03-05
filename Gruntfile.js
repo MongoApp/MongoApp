cc5fe3ba7baab80d0236c46f0210acb887efa2fa
Pavel Kozlov <mail@pkozlov.ru>
2014-03-05 19:33:16 +0100

    no message

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,9 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        curl: {
+            "tmp/mongodb-osx-x86_64-<%= pkg.mongo_version %>.tgz": "http://fastdl.mongodb.org/osx/mongodb-osx-x86_64-<%= pkg.mongo_version %>.tgz",
+        },
         exec: {
             build: {
                 cmd: 'xcodebuild',
